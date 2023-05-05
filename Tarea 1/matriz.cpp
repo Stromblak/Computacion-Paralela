06dcf89b69a66adb118b65f8efc96a0b73762b46
Stromblak <105810066+Stromblak@users.noreply.github.com>
2023-05-05 16:45:51 -0400

    3 a y b listas

--- Tarea 1/matriz.cpp
+++ Tarea 1/matriz.cpp
@@ -15,10 +15,11 @@ void imprimir(vii &M){
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 
-// Matrices tradicionales
+// 1. Matrices tradicionales
 vii mat_trad_sec(vii &A, vii &B){
 	int n = A.size();
 	vector<vector<int>> C(n, vector<int>(n));
@@ -48,7 +49,7 @@ vii mat_trad_par(vii &A, vii &B){
 }
 
 
-// Matrices amigables con el cache
+// 2. Matrices amigables con el cache
 vii mat_amigable_sec(vii &A, vii &B){
 	int n = A.size();
 	vector<vector<int>> C(n, vector<int>(n));
@@ -94,18 +95,14 @@ vii mat_amigable_par(vii &A, vii &B){
 	return C;
 }
 
-// 3a
 
-
-
-// Strassen
-// https://www.youtube.com/watch?v=OSelhO6Qnlc
+// 4. Strassen - https://www.youtube.com/watch?v=OSelhO6Qnlc
 vii suma(vii &M1, vii &M2){
-	int tam = M1.size();
-	vii M(tam, vector<int>(tam));
+	int n = M1.size();
+	vii M(n, vector<int>(n));
 
-	for(int i=0; i<tam; i++){
-		for(int j=0; j<tam; j++){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
 			M[i][j] = M1[i][j] + M2[i][j];
 		}
 	}
@@ -114,70 +111,121 @@ vii suma(vii &M1, vii &M2){
 }
 
 vii resta(vii &M1, vii &M2){
-	int tam = M1.size();
-	vii M(tam, vector<int>(tam));
+	int n = M1.size();
+	vii M(n, vector<int>(n));
 
-	for(int i=0; i<tam; i++){
-		for(int j=0; j<tam; j++){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
 			M[i][j] = M1[i][j] - M2[i][j];
 		}
 	}
 
 	return M;
 }
+ 
+vii multiplicacion_3a_sec(vii A, vii B){
+	if(A.size() <= 2 << 5) return mat_amigable_sec(A, B);
 
-vii strassen(vii A, vii B){
-	// implementar limite de recursion con multiplicacion normal
-	if(A.size() == 1){
-		vii C(1, vector<int>(1));
-		C[0][0] = A[0][0] * B[0][0];
-		return C;
-	}
+	int n = A.size()/2;
 
-	int tam = A.size()/2;
+	vii A11(n, vector<int>(n)), A12(n, vector<int>(n));
+	vii A21(n, vector<int>(n)), A22(n, vector<int>(n));
+	vii B11(n, vector<int>(n)), B12(n, vector<int>(n));
+	vii B21(n, vector<int>(n)), B22(n, vector<int>(n));
 
-	vii A11(tam, vector<int>(tam)), A12(tam, vector<int>(tam));
-	vii A21(tam, vector<int>(tam)), A22(tam, vector<int>(tam));
-	vii B11(tam, vector<int>(tam)), B12(tam, vector<int>(tam));
-	vii B21(tam, vector<int>(tam)), B22(tam, vector<int>(tam));
-
-	for(int i=0; i<tam; i++){
-		for(int j=0; j<tam; j++){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
 			A11[i][j] = A[i][j];
-			A12[i][j] = A[i][j+tam];
-			A21[i][j] = A[i+tam][j];
-			A22[i][j] = A[i+tam][j+tam];
+			A12[i][j] = A[i][j+n];
+			A21[i][j] = A[i+n][j];
+			A22[i][j] = A[i+n][j+n];
 			B11[i][j] = B[i][j];
-			B12[i][j] = B[i][j+tam];
-			B21[i][j] = B[i+tam][j];
-			B22[i][j] = B[i+tam][j+tam];
+			B12[i][j] = B[i][j+n];
+			B21[i][j] = B[i+n][j];
+			B22[i][j] = B[i+n][j+n];
 		}
 	}
 
-	vii M1 = strassen( suma(A11, A22), suma(B11, B22) );
-	vii M2 = strassen( suma(A21, A22), B11 );
-	vii M3 = strassen( A11, resta(B12, B22) );
-	vii M4 = strassen( A22, resta(B21, B11) );
-	vii M5 = strassen( suma(A11, A12), B22 );
-	vii M6 = strassen( resta(A21, A11), suma(B11, B12) );
-	vii M7 = strassen( resta(A12, A22), suma(B21, B22) );
+	vii aux1 = multiplicacion_3a_sec(A11, B11);
+	vii aux2 = multiplicacion_3a_sec(A12, B21);
+	vii C1 = suma(aux1, aux2);
+	
+	aux1 = multiplicacion_3a_sec(A11, B12);
+	aux2 = multiplicacion_3a_sec(A12, B22);	
+	vii C2 = suma(aux1, aux2);
 
-	vii C(tam*2, vector<int>(tam*2));
-	for(int i=0; i<tam; i++){
-		for(int j=0; j<tam; j++){
-			C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
-			C[i][j+tam] = M3[i][j] + M5[i][j];
-			C[i+tam][j] = M2[i][j] + M4[i][j];
-			C[i+tam][j+tam] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+	aux1 = multiplicacion_3a_sec(A21, B11);
+	aux2 = multiplicacion_3a_sec(A22, B21);	
+	vii C3 = suma(aux1, aux2);
+
+	aux1 = multiplicacion_3a_sec(A22, B12);
+	aux2 = multiplicacion_3a_sec(A22, B22);	
+	vii C4 = suma(aux1, aux2);
+
+
+	vii C(n*2, vector<int>(n*2));
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			C[i][j] = C1[i][j];
+			C[i][j+n] = C2[i][j];
+			C[i+n][j] = C3[i][j];
+			C[i+n][j+n] = C4[i][j];
 		}
 	}
-
 
 	return C;
 }
 
+vii strassen_sec(vii A, vii B){
+	// limite recursion
+	if(A.size() <= 2 << 5) return mat_amigable_sec(A, B);
 
+	int n = A.size()/2;
 
+	// Submatrices de A y B
+	vii A11(n, vector<int>(n)), A12(n, vector<int>(n));
+	vii A21(n, vector<int>(n)), A22(n, vector<int>(n));
+	vii B11(n, vector<int>(n)), B12(n, vector<int>(n));
+	vii B21(n, vector<int>(n)), B22(n, vector<int>(n));
+
+	// Particionar las matrices A y B en 4 submatrices cada una
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			A11[i][j] = A[i][j];
+			A12[i][j] = A[i][j+n];
+			A21[i][j] = A[i+n][j];
+			A22[i][j] = A[i+n][j+n];
+			B11[i][j] = B[i][j];
+			B12[i][j] = B[i][j+n];
+			B21[i][j] = B[i+n][j];
+			B22[i][j] = B[i+n][j+n];
+		}
+	}
+
+	vii M1 = strassen_sec( suma(A11, A22), suma(B11, B22) );
+	vii M2 = strassen_sec( suma(A21, A22), B11 );
+	vii M3 = strassen_sec( A11, resta(B12, B22) );
+	vii M4 = strassen_sec( A22, resta(B21, B11) );
+	vii M5 = strassen_sec( suma(A11, A12), B22 );
+	vii M6 = strassen_sec( resta(A21, A11), suma(B11, B12) );
+	vii M7 = strassen_sec( resta(A12, A22), suma(B21, B22) );
+
+	vii C(n*2, vector<int>(n*2));
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+			C[i][j+n] = M3[i][j] + M5[i][j];
+			C[i+n][j] = M2[i][j] + M4[i][j];
+			C[i+n][j+n] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+		}
+	}
+
+	return C;
+}
+
+// 3a paralelizado
+
+// strassen paralelizado
 
 int main(int argc, char *argv[]){
 	minstd_rand rng;
@@ -197,7 +245,6 @@ int main(int argc, char *argv[]){
 	//imprimir(A);
 	//imprimir(B);
 
-
 	int i = 0;
 	while(1){
 		vii C;
@@ -207,15 +254,22 @@ int main(int argc, char *argv[]){
 		else if(i == 1) C = mat_trad_par(A, B);
 		else if(i == 2) C = mat_amigable_sec(A, B);
 		else if(i == 3) C = mat_amigable_par(A, B);
-		else if(i == 4) C = strassen(A, B);
+		else if(i == 4) C = multiplicacion_3a_sec(A, B);
+		else if(i == 5) C = strassen_sec(A, B);
 		else break;
 
 		auto finish = high_resolution_clock::now();
 		auto d = duration_cast<nanoseconds> (finish - start).count();
-		cout <<"total time "<< d << " [ns]" << " \n";
 
-		if(n == 2 && (i == 0 || i == 4) ) imprimir(C);
 
+		if(i == 0) 		cout << "Trad sec     "<< d << " [ns]" << endl;
+		else if(i == 1) cout << "Trad par     "<< d << " [ns]" << endl;
+		else if(i == 2) cout << "Amig sec     "<< d << " [ns]" << endl;
+		else if(i == 3) cout << "Amig par     "<< d << " [ns]" << endl;
+		else if(i == 4) cout << "3a par       "<< d << " [ns]" << endl;
+		else if(i == 5) cout << "Strassen sec "<< d << " [ns]" << endl;
+
+		if(n <= 4) imprimir(C);
 		i++;
 	}
 
